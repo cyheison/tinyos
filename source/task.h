6c281a5cc2f8c37f95512da2c36f1a4ba09f2995
Heison <heiy@cypress.com>
2019-06-28 07:50:53 +0800

    Add task.h

---
+++ source/task.h
@@ -0,0 +1,55 @@
+#ifndef TASK_H
+#define TASK_H
+
+#define TINYOS_TASK_STATE_RDY           0
+#define TINYOS_TASK_STATE_DESTROYED     (1<<1)
+#define TINYOS_TASK_STATE_DELAY         (1<<2)
+#define TINYOS_TASK_STATE_SUSPEND       (1<<3)
+
+typedef enum
+{   
+    noError = 0,
+}errorNum;
+
+typedef struct
+{
+    uint32_t*   stack;
+    uint32_t    systemTickCount;
+    tNode       linkNode; // tasks shoule be added into a list when they have same priority
+    uint32_t    slice;    // When tasks have the same priority, slice number shows that how long they can occupy the CPU. Now the default time is 100ms  
+    uint32_t    pri;
+    tNode       delayNode;
+    uint32_t    suspendCount; // Used to record how many times that this task has been suspended. Usually we only suspend a task once.
+    uint32_t    state;
+    
+    // Used for task clean
+    void (*clean)(void*);
+    void* cleanParam;
+    uint8_t requestDeleteFlag; // Flag for the request of deleting task
+    
+    // Task event
+    tEvent* waitEvent; // This task is waiting for what event
+    void* eventMsg; // Used in mail
+    errorNum eventResult;
+}tTask;
+
+typedef struct 
+{
+    uint32_t delayTicks;
+    uint32_t pri;
+    uint32_t state;
+    uint32_t slice;
+    uint32_t suspendCount;
+}tTaskInfo;
+
+void tTaskInit(tTask *task, void (*entry)(void*), void* param, uint32_t pri, uint32_t *stack);
+void taskSuspend(tTask* task);
+void timedTaskWakeUp(tTask* task);
+void taskRegisterCleanFunc(tTask* task, void (*clean)(void*), void* param);
+void taskForceDelete(tTask* task);
+void taskRequestDelete(tTask* task);
+uint8_t taskRequestDeleteFlag(tTask* task);
+void taskDeleteSelf(tTask* task);
+void taskInfoGet(tTask* task, tTaskInfo* taskInfo);
+
+#endif
--- source/tinyOS.h
+++ source/tinyOS.h
@@ -1,11 +1,11 @@
 #ifndef TINYOS_H
 #define TINYOS_H
 
-#include "stdint.h"
 #include "ARMCM3.h"
 #include "config.h"
 #include "tLib.h"
 #include "event.h"
+#include "task.h"
 
 #define NVIC_INT_CTRL               0xE000Ed04
 #define NVIC_PENDSVSET              0x10000000
@@ -14,46 +14,6 @@
 
 #define mem32(addr)                 *(volatile uint32_t *)(addr)
 //#define mem8(addr)                  *(unsigned char *)(addr)
-
-#define TINYOS_TASK_STATE_RDY           0
-#define TINYOS_TASK_STATE_DELAY         (1<<1)
-#define TINYOS_TASK_STATE_SUSPEND       (1<<2)
-
-typedef enum
-{   
-    noError = 0,
-}errorNum;
-
-typedef struct
-{
-    uint32_t*   stack;
-    uint32_t    systemTickCount;
-    tNode       linkNode; // tasks shoule be added into a list when they have same priority
-    uint32_t    slice;    // When tasks have the same priority, slice number shows that how long they can occupy the CPU. Now the default time is 100ms  
-    uint32_t    pri;
-    tNode       delayNode;
-    uint32_t    suspendCount; // Used to record how many times that this task has been suspended. Usually we only suspend a task once.
-    uint32_t    state;
-    
-    // Used for task clean
-    void (*clean)(void*);
-    void* cleanParam;
-    uint8_t requestDeleteFlag; // Flag for the request of deleting task
-    
-    // Task event
-    tEvent* waitEvent; // This task is waiting for what event
-    void* eventMsg; // Used in mail
-    errorNum eventResult;
-}tTask;
-
-typedef struct 
-{
-    uint32_t delayTicks;
-    uint32_t pri;
-    uint32_t state;
-    uint32_t slice;
-    uint32_t suspendCount;
-}tTaskInfo;
 
 extern tTask * nextTask;
 extern tTask * currentTask;
@@ -71,24 +31,16 @@ void tTaskSchedEnable(void);
 // This schedual function can be invoked by irq or task, so needs to to be protected.
 void tTaskSchedual(void);
 void tTaskSystemTickHandler(void);
-void timedTaskWakeUp(tTask* task);
 void timedTaskWait(tTask* task, uint32_t tick);
-void tTaskInit(tTask *task, void (*entry)(void*), void* param, uint32_t pri, uint32_t *stack);
+
 void tSetSysTickPeriod(uint32_t ms);
 void initApp(void);
 void taskSchedUnReady(tTask* task);
 void taskSchedReady(tTask* task);
 void setTaskDelay(uint32_t delay);
-void taskSuspend(tTask* task);
 void taskWakeupFromSuspend(tTask* task);
 void taskDelayedDelete(tTask* task);
 void taskSchedDelete(tTask* task);
-void taskRegisterCleanFunc(tTask* task, void (*clean)(void*), void* param);
-void taskForceDelete(tTask* task);
-void taskRequestDelete(tTask* task);
-uint8_t taskRequestDeleteFlag(tTask* task);
-void taskDeleteSelf(tTask* task);
-void taskInfoGet(tTask* task, tTaskInfo* taskInfo);
 
 
 #endif
